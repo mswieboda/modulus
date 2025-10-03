a0389ddf9c9607b41c4ad954f536ae8a14445085
Matt Swieboda <mswieboda@users.noreply.github.com>
2025-10-03 18:42:42 -0500

    better camera and ship movement, main menu background

--- objs/ship/ship.gd
+++ objs/ship/ship.gd
@@ -2,17 +2,21 @@ extends CharacterBody3D
 
 # movement
 @export var speed: float = 5.0
+@export var strafe_speed: float = 1.0
+@export var reverse_speed: float = 1.0
 @export var acceleration: float = 10.0
 @export var friction: float = 15.0
 
 func _physics_process(delta: float):
     # input direction
-    var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+    var input_dir := Input.get_vector("strafe_left", "strafe_right", "move_forward", "move_backward")
     var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
     # apply movement with acceleration/friction
     if direction:
-        velocity.x = move_toward(velocity.x, direction.x * speed, acceleration * delta)
+        velocity.x = move_toward(velocity.x, direction.x * strafe_speed, acceleration * delta)
+
+        # TODO: use `reverse_speed` for a negative direction.z, and `speed` for positive
         velocity.z = move_toward(velocity.z, direction.z * speed, acceleration * delta)
     else:
         velocity.x = move_toward(velocity.x, 0, friction * delta)
