e5e1d13da95adcdd7523408206986e1db484d962
Matt Swieboda <mswieboda@users.noreply.github.com>
2025-10-04 18:20:29 -0500

    asteroid collision boxes

--- asteroid_belt.gd
+++ asteroid_belt.gd
@@ -11,27 +11,33 @@ var asteroids = [asteroid_1, asteroid_2, asteroid_3]
 var ice_material = preload("res://objs/asteroids/asteroid_materials/ice_mat.material")
 var rock_material = preload("res://objs/asteroids/asteroid_materials/rock_mat.material")
 var iron_material = preload("res://objs/asteroids/asteroid_materials/iron_mat.material")
-var material = [ice_material, rock_material, iron_material]
+var materials = [ice_material, rock_material, iron_material]
 
 func _ready() -> void:
-  generate()
+    generate()
 
 func generate():
-  var new_asteroid
-  var weight = 0.0
-  var lerp_val = 0.0
-  var spawn_loc = Vector3(0,0,0)
-  while (weight <= end):
-      lerp_val = lerp(start, end, weight)
-      var angle = randf_range(0.0, 360.0)
-      spawn_loc.z = lerp_val
-      spawn_loc.y = randf_range(00.0, 200.0) * sin(angle)
-      spawn_loc.x = randf_range(00.0, 200.0) * cos(angle)
-      var model_choice = randi_range(0,2)
-      var material_choice = randi_range(0,2)
-      new_asteroid = asteroids[model_choice].instantiate()
-      new_asteroid.position = spawn_loc
-      new_asteroid.get_child(0).get_child(0).set_surface_override_material(0, material[material_choice])
-      print(new_asteroid.get_child(0))
-      add_child(new_asteroid)
-      weight += 0.1
+    var weight = 0.0
+
+    while (weight <= end):
+        var lerp_val = lerp(start, end, weight)
+        var angle = randf_range(0.0, 360.0)
+        var spawn_loc = Vector3()
+
+        spawn_loc.z = lerp_val
+        spawn_loc.y = randf_range(0.0, 200.0) * sin(angle)
+        spawn_loc.x = randf_range(0.0, 200.0) * cos(angle)
+
+        var model_choice = randi_range(0, 2)
+        var material_choice = randi_range(0, 2)
+
+        var new_asteroid: StaticBody3D = asteroids[model_choice].instantiate()
+        var mesh: Node3D = new_asteroid.get_node("mesh")
+        var material = mesh.get_child(0)
+
+        new_asteroid.position = spawn_loc
+        material.set_surface_override_material(0, materials[material_choice])
+
+        add_child(new_asteroid)
+
+        weight += 0.1
